a0d95700b5bab4225220738707e7ab6cda5b38e0
axel.Axwelinho <axel.rosatimarzetti@gmail.com>
2023-09-13 15:40:35 +0200

    //

--- Assets/Script/Batiments/ReserveOr.cs
+++ Assets/Script/Batiments/ReserveOr.cs
@@ -1,19 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReserveOr : MonoBehaviour
 {
     public MineOr mineOr;
+    public CameraMovement cameraMovement;
 
+    public Text reserveOrLevelText;
+    public Text reserveOrUpgradeButton;
+    public Text reserveOrTimerBar;
+    public Slider reserveOrSlider;
+    public Image reserveOrTimeBarImage;
+
+    public int limitGold;
+    
+    private bool isFull;
+    private bool isClick;
+
+    private int currentGold;
     void Start()
     {
-        
+        currentGold = mineOr.goldCount;
     }
 
     
     void Update()
     {
-        Debug.Log(mineOr.goldCount);
+        currentGold = mineOr.goldCount;
+        string formatedCurrentGold = currentGold > 10000 ? (currentGold / 1000).ToString() + "K" : currentGold.ToString();
+        CheckGold();
+        Debug.Log(isFull);
+        if (isClick == true)
+        {
+            reserveOrLevelText.text = formatedCurrentGold.ToString();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (isClick == false) 
+        {
+            reserveOrLevelText.gameObject.SetActive(true);
+            cameraMovement.canMove = false;
+            isClick = true;
+        
+        }
+        else
+        {
+            reserveOrLevelText.gameObject.SetActive(false);
+            cameraMovement.canMove = true;
+            isClick = false;
+        }
+    }
+
+    private void CheckGold()
+    {
+        if (currentGold <= limitGold) 
+        {
+            isFull = false;
+        }
+        else  
+        {
+          isFull=true;
+        }
     }
 }
